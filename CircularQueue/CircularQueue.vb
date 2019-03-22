fabeb4c88dde7fa29405d9b7d49cfffddc101ae4
philw <phil@pwood.net>
2019-03-22 14:46:49 +0000

    v1.0 again

---
+++ CircularQueue/CircularQueue.vb
@@ -0,0 +1,40 @@
+﻿Public Class CircularQueue
+
+    Private Const MAX_SIZE = 3
+    Private items(MAX_SIZE - 1) As String
+    Private head As Integer = 0
+    Private tail As Integer = -1
+    Private size As Integer = 0
+
+    Public Function Enqueue(newItem As String) As Boolean
+
+        If size < MAX_SIZE Then
+            tail = (tail + 1) Mod MAX_SIZE
+            items(tail) = newItem
+            size += 1
+            Return True
+        Else
+            Console.WriteLine("ERROR: Queue is full")
+            Return False
+        End If
+
+    End Function
+
+    Public Function Dequeue() As String
+
+        Dim item As String = ""
+
+        If size > 0 Then
+            item = items(head)
+            head = (head + 1) Mod MAX_SIZE
+            size -= 1
+        Else
+            Console.WriteLine("ERROR: Queue is empty")
+        End If
+        Return item
+
+    End Function
+
+
+
+End Class
---
+++ CircularQueue/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim queue As New CircularQueue
+        Dim UserInput As String
+        Dim item As String
+
+        Do
+            Console.WriteLine("    Enqueue")
+            Console.WriteLine("    Dequeue")
+            Console.WriteLine("    eXit")
+            Console.Write("Enter letter: ")
+            UserInput = Console.ReadLine.ToUpper
+
+            Select Case UserInput
+                Case "E"
+                    Console.Write("Enter string: ")
+                    item = Console.ReadLine.Trim
+                    If item <> "" Then
+                        If queue.Enqueue(item) Then
+                            Console.WriteLine("{0} added to the queue", item)
+                        End If
+                    End If
+                Case "D"
+                    item = queue.Dequeue()
+                    If item <> "" Then
+                        Console.WriteLine("Taken {0} off the head of the queue", item)
+                    End If
+            End Select
+
+
+
+        Loop Until UserInput = "X"
+
+
+
+
+    End Sub
+
+End Module
